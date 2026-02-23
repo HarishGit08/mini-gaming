dfc533e0bffbf77219d0de91387c5c68c5cf5045
HarishGit08 <harishlucky2004@gmail.com>
2026-02-23 19:28:32 +0530

    Added PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Ultimate Tic Tac Toe',
+        short_name: 'TicTacToe',
+        description: 'Play Ultimate Tic Tac Toe Online',
+        theme_color: '#111827',
+        background_color: '#111827',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
